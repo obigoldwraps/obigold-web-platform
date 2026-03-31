7d57edb362350527d16ed9c2fe51110030fe095a
obigoldwraps <obigoldwraps@gmail.com>
2026-03-31 22:32:53 +0100

    data

--- src/components/BookingModal.tsx
+++ src/components/BookingModal.tsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { createClient } from '@supabase/supabase-js'; // 1. Import Supabase
 
 // Initialize Supabase Client
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseUrl = import.meta.env.VITE_SUPABASE_DATABASE_URL;
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
